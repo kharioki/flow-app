6b6863a710e81d00e9ac74ad1c4818227475a3ec
kharioki <tony.kharioki@gmail.com>
2022-09-26 22:12:16 +0300

    env vars

--- flow/config.js
+++ flow/config.js
@@ -1,7 +1,7 @@
 import { config } from '@onflow/fcl';
 
 config({
-  "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
-  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
-  "0xProfile": "0xba1132bc08f82fe2" // The account address where the Profile smart contract lives on Testnet
+  "accessNode.api": process.env.NEXT_PUBLIC_ACCESS_NODE_URL, // Mainnet: "https://rest-mainnet.onflow.org"
+  "discovery.wallet": process.env.NEXT_PUBLIC_DISCOVERY_WALLET, // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "0xProfile": process.env.NEXT_PUBLIC_PROFILE // The account address where the Profile smart contract lives on Testnet
 })
